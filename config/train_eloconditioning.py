fe590d97c506115c71c8e7fbfd6ae834369aa0f2
ezhang7423 <ezhang7423@student.palomar.edu>
2024-03-26 03:12:46 +0000

    https://wandb.ai/lad/chess-gpt-batch/runs/w3qpsvav/workspace?nw=nwuserezipe

--- config/train_eloconditioning.py
+++ config/train_eloconditioning.py
@@ -9,7 +9,7 @@ log_interval = 100 # don't print too too often
 # we expect to overfit on this small dataset, so only save when val improves
 always_save_checkpoint = True
 
-wandb_log = False # override via command line if you like
+wandb_log = True # override via command line if you like
 wandb_project = 'chess-gpt-batch'
 wandb_run_name = 'chess-elocondition'
 
